310175ebe0c336f64ca379f9cc18fb1e85e6e665
winniehcy <winniehcyeung@gmail.com>
2020-09-29 23:31:13 -0400

    section-two draft

--- frontend/src/components/SectionTwo.js
+++ frontend/src/components/SectionTwo.js
@@ -3,8 +3,17 @@ import React from 'react';
 function SectionTwo() {
   return (
     <div className="section-two">
-        <img src="./Design_Assets/Screen.png" alt="screen"/>
-        <img src="./Design_Assets/Diamond.png" alt="diamond"/>
+        <h1 className="floating-text">Lorem Ipsum Dolor Lrem Ipsum Dolor Amet.</h1>
+        
+        <img className="screen" src="./Design_Assets/Screen.png" alt="screen"/>
+        <img className="diamond" src="./Design_Assets/Diamond.png" alt="diamond"/>
+        <div className="section-two-text">
+            <p>Lorem Ipsum Dolor Lrem Ipsum Dolor Amet. Lorem Ipsum Dolor Lrem Ipsum Dolor Amet.</p>
+            <p>Ut arcu diam, fermentum sed elit id, condimentum malesuada nunc. Vestibulum convallis enim a mattis aliquet. Nulla nisi augue, porta in lectus in, sodales sagittis justo. </p>        
+        </div>
+        
+        <div className="footer"></div>
+             
     </div>
   );
 }
